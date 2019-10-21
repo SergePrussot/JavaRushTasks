1421eb2e39e28e33c5dc3acde1b7e15cf100f6f7
Serge Prusov <serge.prusov@gmail.com>
2019-10-20 23:41:10 -0400

    0817

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0828/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task08.task0828;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/* 
+Номер месяца
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        Map<String, Integer> map = new HashMap<>();
+        map.put("January", 1);
+        map.put("February", 2);
+        map.put("March", 3);
+        map.put("April", 4);
+        map.put("May", 5);
+        map.put("June", 6);
+        map.put("July", 7);
+        map.put("August", 8);
+        map.put("September", 9);
+        map.put("October", 10);
+        map.put("November", 11);
+        map.put("December", 12);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String month = reader.readLine();
+
+       int number = map.get(month);
+        System.out.printf("%s is the %d month", month, number);
+    }
+}
