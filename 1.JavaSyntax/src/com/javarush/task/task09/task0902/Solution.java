7db7477b64dda7105943cd74142b5b2daf3fdd6f
Serge Prusov <serge.prusov@gmail.com>
2019-10-22 10:38:45 -0400

    0817

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0902/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task09.task0902;
+
+/* 
+И снова StackTrace
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        method1();
+
+    }
+
+    public static String method1() {
+        method2();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+    }
+
+    public static String method2() {
+        method3();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+
+    }
+
+    public static String method3() {
+        method4();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+
+    }
+
+    public static String method4() {
+        method5();
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+
+    }
+
+    public static String method5() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        return stackTraceElements[2].getMethodName();
+
+    }
+}
