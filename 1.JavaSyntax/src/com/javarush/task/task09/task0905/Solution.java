03ba13882eb55a33c3e54d51b006c8b43050264d
Serge Prusov <serge.prusov@gmail.com>
2019-10-22 11:10:34 -0400

    0817

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0905/Solution.java
@@ -0,0 +1,19 @@
+package com.javarush.task.task09.task0905;
+
+/* 
+Там, в синих глубинах стек-трейса…
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int deep = getStackTraceDepth();
+    }
+
+    public static int getStackTraceDepth() {
+        //напишите тут ваш код
+        StackTraceElement[] elements = Thread.currentThread().getStackTrace();
+        System.out.println(elements.length);
+        return elements.length;
+    }
+}
+
