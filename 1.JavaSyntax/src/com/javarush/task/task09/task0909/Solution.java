cc56a8c0a5be5a9df8e9040363f4c77a76206a1f
Serge Prusov <serge.prusov@gmail.com>
2019-10-23 11:21:55 -0400

    Oct. 23, 19

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0909/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task09.task0909;
+
+/* 
+Исключение при работе с массивами
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e.getClass());
+        }
+
+        //напишите тут ваш код
+    }
+}
