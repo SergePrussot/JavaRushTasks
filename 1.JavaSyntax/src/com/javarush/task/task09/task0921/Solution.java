1477a76eec847a2b5586acd1f3c06b1aeb37dc56
Serge Prusov <serge.prusov@gmail.com>
2019-10-25 14:56:27 -0400

    Oct.25,19

---
+++ 1.JavaSyntax/src/com/javarush/task/task09/task0921/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task09.task0921;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/* 
+Метод в try..catch
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        readData();
+    }
+
+    public static void readData() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            try {
+                int i = Integer.parseInt(reader.readLine());
+                list.add(i);
+            } catch (Exception e) {
+                for (Integer i : list
+                ) {
+                    System.out.println(i);
+
+                }
+                break;
+            }
+        }
+    }
+}
