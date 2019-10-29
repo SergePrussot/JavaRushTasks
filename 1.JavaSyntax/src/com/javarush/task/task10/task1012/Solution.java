978b041f813fedccaca89ec1f2526a192416bba2
Serge Prusov <serge.prusov@gmail.com>
2019-10-29 14:37:25 -0400

    Oct.29,19

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1012/Solution.java
@@ -0,0 +1,51 @@
+package com.javarush.task.task10.task1012;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Количество букв
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        // Алфавит
+        List<Character> alphabet = Arrays.asList(
+                'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж',
+                'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о',
+                'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+                'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+
+        // Ввод строк
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            String s = reader.readLine();
+            list.add(s.toLowerCase());
+        }
+
+        char[] chars;
+
+        for (Character c : alphabet
+        ) {
+
+            int count = 0;
+            for (String s : list
+            ) {
+                chars = s.toCharArray();
+                for (char ch : chars
+                ) {
+                    if (ch == c) {
+                        count++;
+                    }
+                }
+
+            }
+            System.out.println(c + " " + count);
+        }
+    }
+}
