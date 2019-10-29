e69eaaf9d64eed6c4602dd59d9f201aef7d9d87a
Serge Prusov <serge.prusov@gmail.com>
2019-10-29 14:07:32 -0400

    Oct.29,19

---
+++ 1.JavaSyntax/src/com/javarush/task/task10/task1011/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task10.task1011;
+
+/* 
+Большая зарплата
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Я не хочу изучать Java, я хочу большую зарплату";
+
+        char[] array = s.toCharArray();
+
+        for (int i = 0; i < 40; i++) {
+            for (int j = i; j < array.length; j++) {
+                System.out.print(array[j]);
+            }
+            System.out.println("");
+        }
+
+    }
+}
+
+
+
