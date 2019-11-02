a9c5c229d3baf30d3f53f8f95ccdbf6294471eea
Serge Prusov <serge.prusov@gmail.com>
2019-11-02 01:46:07 -0400

    Nov.2,19

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+public static void print(int i) {}
+public static void print(String s) {}
+public static void print(char c) {}
+public static void print(long l) {}
+public static void print(float f) {}
+}
