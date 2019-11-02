520f1130028ef674aa446e0d6383fa6325386bce
Serge Prusov <serge.prusov@gmail.com>
2019-11-02 01:42:52 -0400

    Nov.2,19

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        Integer integer = 1;
+        print(i);
+        print(integer);
+
+    }
+
+    public static void print(int i) {}
+    public static void print(Integer integer) {}
+}
