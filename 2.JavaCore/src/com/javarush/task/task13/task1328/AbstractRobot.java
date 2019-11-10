5444a95fc105ba8964be374ab4126d60b0a1ae15
Serge Prusov <serge.prusov@gmail.com>
2019-11-10 01:38:34 -0500

    Nov.10,19

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1328/AbstractRobot.java
@@ -0,0 +1,48 @@
+package com.javarush.task.task13.task1328;
+
+public abstract class AbstractRobot implements Attackable, Defensable {
+
+    private static int hitCount;
+
+
+    public BodyPart attack() {
+        BodyPart attackedBodyPart = null;
+        hitCount = hitCount + 1;
+
+        if (hitCount == 1) {
+            attackedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 2) {
+            attackedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 3) {
+            attackedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 4) {
+            attackedBodyPart = BodyPart.CHEST;
+        }
+        if (hitCount >= 4)
+            hitCount = 0;
+        return attackedBodyPart;
+    }
+
+    public BodyPart defense() {
+        BodyPart defendedBodyPart = null;
+        hitCount = hitCount + 2;
+
+        if (hitCount == 1) {
+            defendedBodyPart = BodyPart.HEAD;
+        } else if (hitCount == 2) {
+            defendedBodyPart = BodyPart.LEG;
+        } else if (hitCount == 3) {
+            defendedBodyPart = BodyPart.ARM;
+        } else if (hitCount == 4) {
+            defendedBodyPart = BodyPart.CHEST;
+        }
+
+        if (hitCount >= 4)
+            hitCount = 0;
+        if (defendedBodyPart == null)
+            return BodyPart.CHEST;
+        return defendedBodyPart;
+    }
+
+
+}
