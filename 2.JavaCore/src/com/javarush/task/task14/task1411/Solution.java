bea2662353eb0a81015c79d74fa4519c50e75728
Serge Prusov <serge.prusov@gmail.com>
2019-11-15 15:31:25 -0500

    Nov.13,19

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,50 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+
+        while (true) {
+            key = reader.readLine();
+            if (key.equals("user"))
+                person = new Person.User();
+            else if (key.equals("loser"))
+                person = new Person.Loser();
+            else if (key.equals("coder"))
+                person = new Person.Coder();
+            else if (key.equals("proger"))
+                person = new Person.Proger();
+            else {
+                reader.close();
+                break;
+            }
+
+            if (person != null)
+                doWork(person);
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        if (person instanceof Person.User)
+            ((Person.User) person).live();
+
+        if (person instanceof Person.Loser)
+            ((Person.Loser) person).doNothing();
+
+        if (person instanceof Person.Coder)
+            ((Person.Coder) person).writeCode();
+
+        if (person instanceof Person.Proger)
+            ((Person.Proger) person).enjoy();
+    }
+}
