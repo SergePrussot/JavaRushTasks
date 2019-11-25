9867469789baaf7dd7e60c00946dab1a9f632c3e
Serge Prusov <serge.prusov@gmail.com>
2019-11-24 23:29:48 -0500

    Nov.24,19

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+
+    private Solution(String s) {
+    }
+
+    protected Solution(int i) {
+    }
+
+     Solution(boolean b) {
+    }
+
+    public Solution() {
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    protected SubSolution(int i) {
+        super(i);
+    }
+
+    SubSolution(boolean b) {
+        super(b);
+    }
+
+    public SubSolution() {
+        super();
+    }
+}
