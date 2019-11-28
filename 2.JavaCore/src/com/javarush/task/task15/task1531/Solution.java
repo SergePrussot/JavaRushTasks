d360198ab39fd866a48932accb232f1341fc24d0
Serge Prusov <serge.prusov@gmail.com>
2019-11-28 03:35:31 -0500

    Nov.28,19

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1531/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task15.task1531;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+/* 
+Факториал
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int input = Integer.parseInt(reader.readLine());
+        reader.close();
+
+        System.out.println(factorial(input));
+    }
+
+    public static String factorial(int n) {
+        BigInteger result = BigInteger.valueOf(1);
+        if (n < 0)
+            return String.valueOf(0);
+        else if (n > 150)
+            return "too big";
+        else if (n == 0)
+            return String.valueOf(1);
+        else {
+            for (int i = 1; i <= n; i++) {
+                result = result.multiply(BigInteger.valueOf(i));
+            }
+        }
+
+        return String.valueOf(result);
+    }
+}
